bac00c31633edba7f18ae3cddae67baa826708b7
Zhihong You <zhihongyou6@gmail.com>
2022-02-04 15:16:39 -0800

    Update random.

--- src/system/systemclass.cpp
+++ src/system/systemclass.cpp
@@ -5,11 +5,11 @@
 using namespace std; 
 
 
-System:: System(double dx,double dy, double dz){
-    // define unit steps that previously defined in the header file. This is replaced by the updated version in grid_access. 
-    dx=Lx/(Nx+0.0);
-    dy=Ly/(Ny+0.0);
-    dz=Lz/(Nz+0.0);
-};
+// System:: System(double dx,double dy, double dz){
+//     // define unit steps that previously defined in the header file. This is replaced by the updated version in grid_access. 
+//     dx=Lx/Nx;
+//     dy=Ly/Ny;
+//     dz=Lz/Nz;
+// };
 
 
--- src/system/systemclass.h
+++ src/system/systemclass.h
@@ -1,5 +1,5 @@
-#ifndef SYSTEMCLASS
-#define SYSTEMCLASS
+#ifndef SYSTEMCLASS_H
+#define SYSTEMCLASS_H
 #include <iostream> 
 #include <vector>
 #include <string>
@@ -9,31 +9,51 @@ using namespace std;
 
 //................Class .................................................
 
-class System{
+class System {
     // define parameters for a mesh. Usually, we only consider a rectanguar mesh since most of the problem can be comformally mapped onto it.
     
     // protected so that one can access on different files.
-    protected:
+    // private:
     
-    // Size of the system in different dimensions.
-    real Lx, Ly, Lz;
-    // Number of grids in different dimensions.
-    int Nx, Ny, Nz;
-    // Number of boundary grids in different dimensions.
-    int Nbx, Nby, Nbz;
-    // Spatial resolution in different directions.
-    real dx, dy, dz;
     
+    // one can call outside of the class.
+    public: 
 
-    public: // one can call outside of the class. 
+    // Dimension of the system.
+    int Dimension=1;
+    // Size of the system in different dimensions.
+    double Lx=2, Ly=1, Lz=1;
+    // Number of grids in different dimensions.
+    int Nx=64, Ny=1, Nz=1;
+    // Number of boundary grids in different dimensions.
+    int Nbx=2, Nby=2, Nbz=2;
+    // Spatial resolution in different directions.
+    double Dx, Dy, Dz;
+
     
-    // Define Constructors (length x ,length y, density)
-    System(real Lx=1, real Ly=1, real Lz=1, int Nx=60, int Ny=1, int Nz=1);
+    // Constructor, sets the default values of parameters
+    System (int dimension=1, double lx=1, double ly=1, double lz=1, int nx=64, int ny=1, int nz=1, int nbx=2, int nby=2, int nbz=2) {
+        Dimension=dimension;
+        Lx=lx;
+        Ly=ly;
+        Lz=lz;
+        Nx=nx;
+        Ny=ny;
+        Nz=nz;
+        Nbx=nbx;
+        Nby=nby;
+        Nbz=nbz;
+        Dx=Lx/Nx;
+        Dy=Ly/Ny;
+        Dz=Lz/Nz;
+    };
 
 
 //................Methods ................................................
 
     
 };
+
+
 
 #endif
