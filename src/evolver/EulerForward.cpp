8f493e571ca82e741e20c8a31d9aca2336a5304a
Zhihong You <zhihongyou6@gmail.com>
2022-02-03 20:08:06 -0800

    Add subclasses of Evolver.

---
+++ src/evolver/EulerForward.cpp
@@ -0,0 +1,14 @@
+#include "evolverclass.h"
+#include <iostream> 
+#include <vector>
+
+using namespace std; 
+
+
+// Evolver:: evolver(double dx,double dy, double dz){
+//     dx=Lx/(Nx+0.0);
+//     dy=Ly/(Ny+0.0);
+//     dz=Lz/(Nz+0.0);
+// };
+
+
